069d88546fbec0cc323eefd1916aaf51faca1cf5
Stavros Gkounis <59849433+stavrosgns@users.noreply.github.com>
2024-04-10 12:56:01 +0300

    Implementing The Code For SHIFT + Click Events
    This made deal with the imposter syndrome and code is a little messy!
    It has the correct behaviour most of the time.
    
    One case is needed to be corrected. If I select a single element and then
    shift-click an element above it, it selects all the elements above it. If
    I shift-click element below it afterwards, it keeps selected the initially selected
    and all the element below it.
    
    The problem raises when I first shift-select element below the initially selected
    element, it selects all the element below it. No problem here.
    And yet, if I shift-click an element above it, it keeps selected the previous elements
    plus any element above it.

--- js/eventHandler.js
+++ js/eventHandler.js
@@ -3,11 +3,45 @@ function toggleSelectedCSSClass(li){
 }
 
 function multipleSelectedItems(li){
+    
     const allListItems = document.querySelectorAll('li');
     for(const item of allListItems){
         item.classList.remove('selected');
     }
     li.classList.add('selected');
+}
+
+function shiftSelection(li){
+    const nodeListOfAllItems = document.querySelectorAll('li');
+    const arrayofListItems = Array.from(nodeListOfAllItems);
+    
+    let indexOfSelectedItem = 0; // If there is no selected items consider index = 0 as the first element. Otherwise, the value will change in the for-loop.
+    let toIndex;
+
+    for(const index in arrayofListItems){
+        if( arrayofListItems[index].className === "selected"){
+            indexOfSelectedItem = index;
+        }
+
+        if( arrayofListItems[index].innerText === li.innerText){
+            toIndex = index;
+        }
+        arrayofListItems[index].classList.remove('selected');
+    }
+
+    let from;
+    let to;
+    if( indexOfSelectedItem > toIndex ){
+        from = toIndex;
+        to = indexOfSelectedItem;
+    } else {
+        from = indexOfSelectedItem;
+        to = toIndex;
+    }
+
+    for(; from <= to; from++){
+        arrayofListItems[from].classList.add('selected');
+    }
 }
 
 const myList = document.querySelector("#myList");
@@ -17,7 +51,7 @@ myList.onclick = (event) => {
     if( event.ctrlKey || event.metaKey ){
         toggleSelectedCSSClass(event.target);
     } else if ( event.shiftKey ){
-        console.log("SHIFT + Click Event {TODO}");
+        shiftSelection(event.target);
     } else {
         multipleSelectedItems(event.target);
     }
